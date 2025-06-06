acef579546e8305efd4484942761bd4022beb4da
rmd09 <rmd09@yandex.ru>
2025-06-06 22:58:16 +0300

    init
    Сделана основная страница (без карусели профессий), каркас архитектуры

---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import styles from "./Header.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Header = () => {
+    const path = usePathname();
+
+    return(
+        <header className={styles["header"]}>
+            <Link href={"/"}>
+                <img className={styles["logo"]} src="logo.svg" alt="logo" />
+            </Link>
+            <div className={styles["buttons"]}>
+                <Link href={"/test"} className={`${styles["but"]} ${path == "/test" && styles["but-cliked"]}`}>Пройти тест</Link>
+                <Link href={"/jobs"} className={`${styles["but"]} ${path == "/jobs" && styles["but-cliked"]}`}>Смотреть профессии</Link>
+                <Link href={"/colleges"} className={`${styles["but"]} ${path == "/colleges" && styles["but-cliked"]}`}>Смотреть коледжи</Link>
+            </div>
+        </header>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,95 +1,15 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className={styles["main"]}>
+      <h2 className={styles["h2"]}>Найди свою профессию в агропромышленном комплексе!</h2>
+      <h1 className={styles["h1"]}>Почему АПК?</h1>
+      <div className={styles["pluses__container"]}>
+        <h6 className={styles["h6"]}>87% профессий АПК требуют IT-навыков</h6>
+        <h6 className={styles["h6"]}>Зарплата биотехнолога: от 80 тыс. руб. на старте</h6>
+        <h6 className={styles["h6"]}>300+ учебных программ в РФ</h6>
+      </div>
+    </main>
   );
 }
